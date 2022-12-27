2a279bff393903b5423a041bb0b1b6e3e5595535
Rami Elsayed <ramielsayed123@outlook.com>
2022-12-27 13:54:07 +0000

    lifting state up to Board

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,22 +1,30 @@
 import {Square} from "../Square/index.js";
+import {useState} from "react";
 
 export const Board = () => {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    
+    const handleClick = (i) => {
+        const nextSquares = squares.slice();
+        nextSquares[i] = 'X';
+        setSquares(nextSquares)
+    }
     return (
         <>
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                <Square values={squares[0]} onSquareClick={() => handleClick(0)}/>
+                <Square values={squares[1]} onSquareClick={() => handleClick(1)}/>
+                <Square values={squares[2]} onSquareClick={() => handleClick(2)} />
             </div>
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                <Square values={squares[3]} onSquareClick={() => handleClick(3)}/>
+                <Square values={squares[4]} onSquareClick={() => handleClick(4)}/>
+                <Square values={squares[5]} onSquareClick={() => handleClick(5)}/>
             </div>
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                <Square values={squares[6]} onSquareClick={() => handleClick(6)}/>
+                <Square values={squares[7]} onSquareClick={() => handleClick(7)}/>
+                <Square values={squares[8]} onSquareClick={() => handleClick(8)}/>
             </div>
         </>
     )
--- src/components/Square/index.js
+++ src/components/Square/index.js
@@ -1,9 +1,7 @@
-import {useState} from "react";
-
-export const Square = () => {
-    const [value, setValue] = useState(null);
-    const handleClick = () => {
-        setValue('X');
-    }
-    return (<button className="square" onClick={handleClick} >{value}</button>);
+export const Square = ({value, onSquareClick}) => {
+    return (
+        <button className="square" onClick={onSquareClick}>
+            {value}
+        </button>
+    );
 };
