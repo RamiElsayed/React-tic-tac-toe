a00582fc9562c73540c016d90bfb71dd0cbd06d1
Rami Elsayed <ramielsayed123@outlook.com>
2022-12-27 16:08:01 +0000

    calculate winner

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -2,13 +2,23 @@ import {Square} from "../Square/index.js";
 import {useState} from "react";
 
 export const Board = () => {
+    const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
-    
+
     const handleClick = (i) => {
+        if (squares[i] || calculateWinner(squares)){
+            return;
+        }
         const nextSquares = squares.slice();
-        nextSquares[i] = 'X';
-        setSquares(nextSquares)
+        if (xIsNext){
+            nextSquares[i] = 'X';
+        } else{
+            nextSquares[i] = 'O';
+        }
+        setSquares(nextSquares);
+        setXIsNext(!xIsNext);
     }
+
     return (
         <>
             <div className="board-row">
@@ -29,3 +39,22 @@ export const Board = () => {
         </>
     )
 };
+const calculateWinner = (squares) => {
+    const lines =[
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+            return squares[a];
+        }
+        return null;
+    }
+}
